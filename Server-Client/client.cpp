472d0f8c24a6e783eb3003b895f79621adcd0bc4
Robert <sirob0210@gmail.com>
2020-02-27 11:46:45 +0100

    Auto stash before merge of "master" and "origin/master"

--- Server-Client/client.cpp
+++ Server-Client/client.cpp
@@ -3,11 +3,12 @@
 #include <arpa/inet.h> 
 #include <unistd.h> 
 #include <string.h> 
-#define PORT 80
+#define PORT 43210
 
 int main() { 
     int socketDescriptor = 0;
     int valread;
+    const char* IP = "192.168.2.117";
 
     //structure for handling internet addresses
     struct sockaddr_in serv_addr; 
@@ -22,20 +23,23 @@ int main() {
         perror("ERROR PROBLEM WITH SOCKET "); 
         return -1; 
     } 
+    printf("socket created\n");
    
     serv_addr.sin_family = AF_INET; 
     serv_addr.sin_port = htons(PORT); 
        
     // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "192.168.2.117", &serv_addr.sin_addr) <= 0){ 
+    if(inet_pton(AF_INET, IP, &serv_addr.sin_addr) <= 0){ 
         perror("ERROR PROBLEM WITH ADDRESS "); 
         return -1; 
     } 
+    printf("Adress converted \n");
    
     if (connect(socketDescriptor, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){ 
         perror("ERROR PROBLEM WITH Connection "); 
         return -1; 
     } 
+    printf("connection created\n");
 
     //sending Client messsage
     send(socketDescriptor , hello , strlen(hello) , 0 ); 
--- Server-Client/server.cpp
+++ Server-Client/server.cpp
@@ -4,7 +4,7 @@
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
-#define PORT 4321
+#define PORT 43210
 
 
 
@@ -23,33 +23,40 @@ int main(){
     if ((serverFileDescriptor = socket(AF_INET, SOCK_STREAM, 0)) == 0){ 
         perror("ERROR PROBLEM WITH SOCKET ");
     } 
+    printf("socket created\n");
        
-    // Forcefully attaching socket to the port 80 
+    // Forcefully attaching socket to the port 43210 
     if (setsockopt(serverFileDescriptor, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
         perror("ERROR PROBLEM WITH SETSOCKETOPT ");
     } 
+    printf("Attaching Socket to port: %d \n",PORT);
 
     address.sin_family = AF_INET; 
     address.sin_addr.s_addr = INADDR_ANY; 
     address.sin_port = htons( PORT ); 
        
-    // Forcefully attaching socket to the port 8080 
+    // Forcefully attaching socket to the port 80 
     if (bind(serverFileDescriptor, (struct sockaddr *)&address,sizeof(address)) < 0) { 
         perror("ERROR PROBLEM WITH BIND ");
     } 
+    printf("Binding created\n");
+
 
     //listen and waiting for the client to approach
     if (listen(serverFileDescriptor, 3) < 0){ 
         perror("ERROR PROBLEM WITH listen "); 
     } 
+    printf("listening started\n");
 
     //connection is established between client and server, and they are ready to transfer data.
     if ((new_socket = accept(serverFileDescriptor, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0){ 
         perror("ERROR PROBLEM WITH Accept");  
     } 
+    printf("ready to transfer data\n");
 
     //reading client message
     valread = read( new_socket , buffer, 1024); 
+    
     
     //output mesage
     printf("%s\n",buffer ); 
